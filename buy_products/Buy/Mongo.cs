715781edc97ec5a296ce7da4cb817a4d9c195bb3
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2022-02-28 15:56:39 +0100

    Cambios compra

---
+++ buy_products/Buy/Mongo.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+
+namespace Buy
+{
+    public class Mongo
+    {
+        private readonly IMongoCollection<Product> _products;
+
+        public Mongo(MongoClient mongo, string database, string collection)
+        {
+            _products = mongo.GetDatabase(database).GetCollection<Product>(collection);
+        }
+        public Product GetProduct(int id)
+        {
+            return _products.Find(product => product.Id == id).FirstOrDefault();
+        }
+
+        public bool ReduceStockProduct(int id, int amountReduced)
+        {
+            return _products.UpdateOne(product => product.Id == id && product.Stock >= amountReduced,
+                Builders<Product>.Update.Inc(product => product.Stock, -amountReduced)).MatchedCount > 0;
+        }
+    }
+}
---
+++ buy_products/Buy/Product.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Buy
+{
+    public class Product
+    {
+        [BsonId]
+        public int Id;
+
+        [BsonElement("title")]
+        public string Title = string.Empty;
+
+        [BsonElement("price")]
+        public float Price;
+
+        [BsonElement("description")]
+        public string Description = string.Empty;
+
+        [BsonElement("category")]
+        public string Category = string.Empty;
+
+        [BsonElement("image")]
+        public string Image = string.Empty;
+
+        [BsonElement("manual")]
+        public bool Manual;
+
+        [BsonElement("stock")]
+        public int Stock;
+
+        [BsonElement("favorites")]
+        public int Favorites;
+    }
+}
